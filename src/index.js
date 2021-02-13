364f02a5b416dcdb8edea1d7c0cd761fca352102
KononOleg <kononoleg97@gmail.com>
2021-02-13 19:41:15 +0300

    fix: function min, max

--- src/index.js
+++ src/index.js
@@ -1,4 +1,51 @@
-module.exports = function reverse (n) {
-      n= Math.abs(n);
-      return Number(String(n).split('').reverse().join(''));
+
+exports.min = function min (array) {
+
+  if(array.length==0 || array === undefined)
+  {
+    return 0;
+  }
+  else 
+  {
+  	var min = array[0];
+		for(var i = 0; i < array.length; i++){
+			if (array[i] < min) {
+				min = array[i];
+			}
+      return min;
+  }
+}
+
+exports.max = function max (array) {
+  if(array.length==0 || array === undefined)
+  {fds
+    return 0;
+  }
+  else 
+  {
+    var max = array[0];
+		for(var i = 0; i < array.length; i++){
+			if (array[i] > max) {
+				max = array[i];
+			}
+      return max;
+  }
+}
+
+exports.avg = function avg (array) {
+
+      if(array.length==0 || array === undefined)
+      {
+        return 0;
+      }
+      else 
+      {
+        var sum=0;
+        for(var i=0;i<array.length;i++)
+        {
+            sum=+array[i];
+        }
+        return sum/array.length;
+      }
+
 }
